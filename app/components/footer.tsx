4f58eefa3561b71803c2703ddc28b39627b3d59a
{haru-astro} <{hayakawa14959@gmail.com}>
2024-05-13 14:28:28 +0900

    change footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -29,7 +29,7 @@ export default function Footer() {
             target="_blank"
             href="https://twitter.com/haru_astro_"
           >
-            <Image src="/images/Twitter.png" width={20} height={20} alt="Twitter" />
+            <Image src="https://github.com/haru-astro/portfolio/blob/main/public/images/Twitter.png" width={20} height={20} alt="Twitter" />
           </a>
         </li>
         <li>
@@ -39,7 +39,7 @@ export default function Footer() {
             target="_blank"
             href="https://www.instagram.com/haru_nightsky/"
           >
-            <Image src="/images/Instagram.png" width={20} height={20} alt="Instagram" />
+            <Image src="https://github.com/haru-astro/portfolio/blob/main/public/images/Instagram.png" width={20} height={20} alt="Instagram" />
           </a>
         </li>
         <li>
@@ -49,7 +49,7 @@ export default function Footer() {
             target="_blank"
             href="https://note.com/haru_astro/all"
           >
-            <Image src="/images/note.png" width={20} height={20} alt="note" />
+            <Image src="https://github.com/haru-astro/portfolio/blob/main/public/images/note.png?raw=true" width={20} height={20} alt="note" />
           </a>
         </li>
         <li>
@@ -59,7 +59,7 @@ export default function Footer() {
             target="_blank"
             href="https://github.com/haru-astro"
           >
-            <Image src="/images/github.png" width={20} height={20} alt="Instagram" />
+            <Image src="https://github.com/haru-astro/portfolio/blob/main/public/images/github.png" width={20} height={20} alt="Instagram" />
           </a>
         </li>
       </ul>
